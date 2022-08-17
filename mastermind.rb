d342b3425ea8f1e2005e660717b644c8fb6987cd
soadaa <Soadhlh@gmail.com>
2022-08-17 17:05:42 -0400

    Update valid_code? method
    Error is now fixed, @code = Array.new wasn't executed before @code was being called on (which was why ther error refrenced NilClass). Unfortunately valid_code seems to never be called as my puts test isn't seen in the terminal. The program exits out without any stack trace error.

--- mastermind.rb
+++ mastermind.rb
@@ -7,12 +7,10 @@ class Mastermind
     setup_code
   end
 
-  @player = ''
-  @computer = ''
-  @code = Array.new
-  @valid_colors = ['r','b','g','pu','o','pi']
 
   def player_computer_roles
+    @player = ''
+    @computer = ''
     puts 'Do you want to be the Code Master(1) or the Guesser(2)?'
     answer = gets.chomp.to_i
     if answer != 1 && answer != 2
@@ -20,6 +18,7 @@ class Mastermind
       player_computer_roles
     elsif answer == 1
       puts 'You chose Code Master.'
+      puts "\n"
       @player = :code_master
       @computer = :guesser
     elsif answer == 2
@@ -32,32 +31,48 @@ class Mastermind
   end
 
   def setup_code()
+    @code = Array.new
     if @player == :code_master
-      until valid_code?(@code)
+      until valid_code?(@code) do
+        # sleep(2)
         puts 'Your secret code will be a length  of four inputs, with the option'
         puts 'of 6 diffrent colors. Input the intial(s) of the color.'
+        # sleep (5)
         puts 'Red (r), Blue (b), Green (g), Purple (pu), Orange (o), and Pink (pi).'
+        puts "\n"
+        # sleep(2.5)
         puts "Input the first code color:"
-        @code[1] = gets.chomp.downcase
+        @code[0] = gets.chomp.downcase
         puts "Input the second code color:"
-        @code[2] = gets.chomp.downcase
+        @code[1] = gets.chomp.downcase
         puts "Input the third code color:"
-        @code[3] = gets.chomp.downcase
+        @code[2] = gets.chomp.downcase
         puts "Input the fourth code color:"
-        @code[4] = gets.chomp.downcase
-        valid_code?(@code)
+        @code[3] = gets.chomp.downcase
+        # valid_code?(@code)
+        puts @code
       end
     else
-      
+      puts 'Setting up code...'
+      # sleep(0.5)
+      puts 'Please wait while our top CPU\'s pick only the hardest to decipher code.'
+      # sleep(0.8)
+      puts 'This may take anywhere from a few seconds to a few hours depending on your altitude.'
+      # sleep(3)
+      puts "..."
+      # sleep(1)
+      computer_choose_code
     end
   end
 
   def valid_code?(code)
-    if @code.all? { |key, color| @valid_colors.include?(color) }
-      puts "test"
+    @valid_colors = ['r','b','g','pu','o','pi']
+    if !code.empty? && code.all? { |color| @valid_colors.include?(color) }
       return true
+      puts "success"
     else
       return false
+      puts "false"
     end
   end
 end
