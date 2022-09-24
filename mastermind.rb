f835c5915efe3b8a4525866235abd4854c413483
soadaa <Soadhlh@gmail.com>
2022-09-24 15:35:45 -0400

    Incompletely add give_player_hints def
    This will fix the infinite loop bug and now correctly check if player input
    is equal to the code and if not, clear the guess and ask for a new guess.

--- mastermind.rb
+++ mastermind.rb
@@ -9,6 +9,8 @@ class Mastermind
   @@guess = Array.new
   @@attempt_counter = 0
   @@game_over = false
+  @@correct_spot_and_color_guess = 0
+  @@correct_color_guess = 0
   @player = ''
   @computer = ''
   @current_guess_of_code = Array.new
@@ -21,7 +23,7 @@ class Mastermind
 
 
   def player_computer_roles
-    puts 'Do you want to be the Code Master(1) or the Guesser(2)?'
+    puts 'Do you want to be the Code Master(1) or the Code Breaker(2)?'
     answer = gets.chomp.to_i
     if answer != 1 && answer != 2
       puts 'Invalid ouput, try again.'
@@ -30,10 +32,10 @@ class Mastermind
       puts 'You chose Code Master.'
       puts "\n"
       @player = :code_master
-      @computer = :guesser
+      @computer = :code_breaker
     elsif answer == 2
-      puts 'You chose Guesser.'
-      @player = :guesser
+      puts 'You chose Code Breaker.'
+      @player = :code_breaker
       @computer = :code_master
     else
       puts 'Error -.-'
@@ -95,8 +97,9 @@ class Mastermind
   end
 
   def guess_code
-    if @player == :guesser
+    if @player == :code_breaker
       puts "\n"
+      puts 'You will have 6 colors to choose from(red, blue, green, purple, orange and pink).'
       player_to_guess_code
     else
       puts "\n"
@@ -106,7 +109,6 @@ class Mastermind
   end
 
   def player_to_guess_code
-    puts 'You will have 6 colors to choose from (red, blue, green, purple, orange and pink).'
     attempt_number_counter
     if @@game_over == false
       until valid_guess? do
@@ -121,7 +123,6 @@ class Mastermind
         @@guess[3] = gets.chomp.downcase
         end
     end
-    binding.pry
     check_guess
   end
 
@@ -151,9 +152,30 @@ class Mastermind
     game_over_victory
    else
     puts "Guess number ##{@@attempt_counter}: #{@@guess}, was wrong. Try again"
-    @@guess == Array.new
+    give_player_hints
+    @@guess = Array.new
     player_to_guess_code
+    binding.pry
    end
+  end
+
+  def give_player_hints
+    p @@code
+    i = 0
+    binding.pry
+    while i < @@code_length
+      if @@guess[i] == @@code[i]
+        @@correct_spot_and_color_guess += 1
+      end
+      if @@guess.all? { |guess| @@code.include? guess }
+        @@correct_color_guess += 1
+      end
+      # disinclude any positive results from correct guesses involving the placement and color
+      @@correct_color_guess = @@correct_color_guess - @@correct_spot_and_color_guess
+      i += 1
+    end
+    puts "You have #{@@correct_spot_and_color_guess} guesses with both the correct placement and color of the code."
+    puts "Subsequently, you have #{@@correct_color_guess} guesses with only the color correct, not their placements."
   end
 
   def computer_to_guess_code
@@ -161,11 +183,11 @@ class Mastermind
   end
   
   def computer_code_cracker
-
+    p 'Tell Kevin to add code here so stuff works'
   end
 
   def computer_code_cracker_attempt
-    
+    p 'This is an attempt by the pc to hack the mainframes.'
   end
 
   def game_over_failure
